61b72a07b8a0e996126c43899d9180a453311414
lisam03 <91112587+lisam03@users.noreply.github.com>
2024-01-01 23:07:48 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,48 +1,39 @@
 CREATE TABLE drivers
 (
-  driverId INT NOT NULL,
-  number INT,
-  forename VARCHAR(100) NOT NULL,
-  surname VARCHAR(100) NOT NULL,
-  nationality VARCHAR(100),
+  driver_id INT NOT NULL,
+  driver_forename VARCHAR(100) NOT NULL,
+  driver_surname VARCHAR(100) NOT NULL,
+  driver_number VARCHAR(20) NOT NULL,
+  driver_nationality VARCHAR(100),
   PRIMARY KEY (driver_id)
 );
 
 CREATE TABLE constructors
 (
-  constructorId INT NOT NULL,
-  name VARCHAR(100) NOT NULL,
+  constructor_id INT NOT NULL,
+  constructor_name VARCHAR(100) NOT NULL,
   PRIMARY KEY (constructor_id)
 );
 
 CREATE TABLE races
 (
-  raceId INT NOT NULL,
-  year INT NOT NULL,
-  name VARCHAR(100) NOT NULL,
+  race_id INT NOT NULL,
+  race_year INT NOT NULL,
+  race_name VARCHAR(100) NOT NULL,
   PRIMARY KEY (race_id)
 );
 
 CREATE TABLE results
 (
-  resultId INT NOT NULL,
-  raceId INT NOT NULL,
-  driverId INT NOT NULL,
-  constructorId INT NOT NULL,
-  position INT NOT NULL,
-  points INT NOT NULL,
+  result_id INT NOT NULL,
+  race_id INT NOT NULL,
+  driver_id INT NOT NULL,
+  constructor_id INT NOT NULL,
+  final_position VARCHAR(20) NOT NULL,
+  points FLOAT NOT NULL,
   PRIMARY KEY (result_id),
   FOREIGN KEY (constructor_id) REFERENCES constructors(constructor_id),
   FOREIGN KEY (driver_id) REFERENCES drivers(driver_id),
   FOREIGN KEY (race_id) REFERENCES races(race_id)
 );
-
-
-
-
-
-
-
-
-
 
