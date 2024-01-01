44ab051208594427d0e81cfb783a326d802e102c
lisam03 <91112587+lisam03@users.noreply.github.com>
2024-01-01 23:06:50 +0200

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,15 +1,19 @@
--- Функція повертає всі команди, що набрали кількість балів, меншу за вказану
+-- Функція повертає всі команди з сезону 2023-го року, що набрали кількість балів, меншу за вказану
+DROP FUNCTION get_teams_with_points_below_threshold(integer);
+
 CREATE OR REPLACE FUNCTION get_teams_with_points_below_threshold(p_threshold INT)
-RETURNS TABLE (team_name VARCHAR) AS
+RETURNS TABLE (team_name VARCHAR, total_points INT) AS
 $$
 BEGIN
     RETURN QUERY
-    SELECT constructors.constructor_name AS team_name
-    FROM constructors
-    LEFT JOIN results ON constructors.constructor_id = results.constructor_id
+    SELECT constructors.constructor_name AS team_name, COALESCE(SUM(results.points), 0)::INT AS total_points
+    	FROM results INNER JOIN constructors ON results.constructor_id = constructors.constructor_id 
     GROUP BY constructors.constructor_name
-    HAVING COALESCE(SUM(results.points), 0) < p_threshold;
+    HAVING COALESCE(SUM(results.points), 0) < p_threshold
+	ORDER BY total_points DESC;
 END;
 $$ LANGUAGE plpgsql;
 
 SELECT * FROM get_teams_with_points_below_threshold(100);
+
+
